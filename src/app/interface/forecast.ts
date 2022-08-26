9bf99882bcd67834f829652e109e2cb42835b9f1
Lucas Renó <lucasreno9@gmail.com>
2022-08-26 19:52:21 -0300

    Criação das interfaces

---
+++ src/app/interface/forecast.ts
@@ -0,0 +1,8 @@
+export interface Forecast {
+    date: string;
+    weekday: string;
+    max: number;
+    min: number;
+    description: string;
+    condition: string;
+}
---
+++ src/app/interface/results.ts
@@ -0,0 +1,20 @@
+import { Forecast } from "./forecast";
+
+export interface Results {
+    temp: number;
+    date: string;
+    time: string;
+    condition_code: string;
+    description: string;
+    currently: string;
+    cid: string;
+    city: string;
+    img_id: string;
+    humidity: number;
+    wind_speedy: string;
+    sunrise: string;
+    sunset: string;
+    condition_slug: string;
+    city_name: string;
+    forecast: Forecast[];
+}
